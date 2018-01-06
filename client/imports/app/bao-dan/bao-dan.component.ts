c94a71c31e6a857643e9108f92e15b01fb6241fd
Harley Ren <renhuailin@gmail.com>
2018-01-06 11:28:23 +0800

    upload files successfully

--- client/imports/app/bao-dan/bao-dan.component.ts
+++ client/imports/app/bao-dan/bao-dan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Images } from '../../../../imports/collections/images';
 
 @Component({
     selector: 'app-bao-dan',
@@ -12,4 +13,30 @@ export class BaoDanComponent implements OnInit {
     ngOnInit() {
     }
 
+
+    onFileChange(e: any, name: string) {
+        console.log(event);
+        if (e.currentTarget.files && e.currentTarget.files[0]) {
+            // We upload only one file, in case
+            // there was multiple files selected
+            var file = e.currentTarget.files[0];
+            if (file) {
+                var uploadInstance = Images.insert({
+                    file: file,
+                    streams: 'dynamic',
+                    chunkSize: 'dynamic'
+                }, false);
+
+                uploadInstance.on('end', function (error, fileObj) {
+                    if (error) {
+                        window.alert('Error during upload: ' + error.reason);
+                    } else {
+                        window.alert('File "' + fileObj.name + '" successfully uploaded');
+                    }
+                });
+
+                uploadInstance.start();
+            }
+        }
+    }
 }
--- client/imports/app/todo-list/todo-list.component.ts
+++ client/imports/app/todo-list/todo-list.component.ts
@@ -6,6 +6,7 @@ import { MeteorObservable } from 'meteor-rxjs';
 
 import { Todos } from '../../../../imports/collections/todos';
 import { Todo } from '../../../../imports/models/todo';
+import { Images } from '../../../../imports/collections/images';
 
 @Component({
     selector: 'todo-list',
@@ -15,16 +16,35 @@ import { Todo } from '../../../../imports/models/todo';
 export class TodoListComponent implements OnInit, OnDestroy {
     todos: Observable<Todo[]>;
     todoListSubscription: Subscription;
+
+    images: Observable<any>;
+    imagesSubscription: Subscription;
+
     checked: boolean;
     ngOnInit() {
         console.log("User id =  " + Meteor.userId());
         this.todoListSubscription = MeteorObservable.subscribe('todoList').subscribe(() => {
             this.todos = Todos.find();
         });
+
+        this.imagesSubscription = MeteorObservable.subscribe('files.images.all').subscribe(
+            () => {
+                this.images = Images.find().cursor.map(image => {
+                    image.link = Images.link(image);
+                    // console.log(image.link);
+                    return image;
+                });
+            }
+        );
+
     }
     ngOnDestroy() {
         if (this.todoListSubscription) {
             this.todoListSubscription.unsubscribe();
+        }
+
+        if (this.imagesSubscription) {
+            this.imagesSubscription.unsubscribe();
         }
     }
     removeTodo(_id: string) {
@@ -35,4 +55,31 @@ export class TodoListComponent implements OnInit, OnDestroy {
         console.log("Call server login in browser.");
         Meteor.call('doLogin');
     }
+
+    onFileChange(e: any) {
+        console.log(event);
+        if (e.currentTarget.files && e.currentTarget.files[0]) {
+            // We upload only one file, in case
+            // there was multiple files selected
+            var file = e.currentTarget.files[0];
+            if (file) {
+                var uploadInstance = Images.insert({
+                    file: file,
+                    streams: 'dynamic',
+                    chunkSize: 'dynamic'
+                }, false);
+
+                uploadInstance.on('end', function (error, fileObj) {
+                    if (error) {
+                        window.alert('Error during upload: ' + error.reason);
+                    } else {
+                        window.alert('File "' + fileObj.name + '" successfully uploaded');
+                    }
+                });
+
+                uploadInstance.start();
+            }
+        }
+    }
 }
+
